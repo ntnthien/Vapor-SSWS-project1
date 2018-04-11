dced5e4f917ed25377e171b8c40f237ddcc2593f
Do Nguyen <daniel@zincer.com>
2018-04-11 13:41:24 +0700

    ADD basic routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,26 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
+    
+    // MARK: Root route
+    router.get { _ in
+        return "[Root] Welcome to Vapor - Do Nguyen"
+    }
+    
+    // MARK: /hello
     router.get("hello") { req in
         return "Hello, world!"
+    }
+    
+    // MARK: /staff
+
+    router.get("staff") { req in
+        return "Meet our great team"
+    }
+    
+    // MARK: /contact
+
+    router.get("contact") { req in
+        return "Get in touch with us"
     }
 }
